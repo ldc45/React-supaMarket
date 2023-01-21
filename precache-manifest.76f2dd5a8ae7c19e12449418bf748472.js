d77892d2cbf5745d554496a68855347bf030e685
AGESILAS Ludovic <ldc45@outlook.fr>
2023-01-21 12:05:14 +0100

    Updates

---
+++ precache-manifest.76f2dd5a8ae7c19e12449418bf748472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a050ec018c5dc668cf8",
+    "url": "/React-supaMarket/static/css/main.d6acc4c0.chunk.css"
+  },
+  {
+    "revision": "8a050ec018c5dc668cf8",
+    "url": "/React-supaMarket/static/js/main.8a050ec0.chunk.js"
+  },
+  {
+    "revision": "28cdc5a0ba160ec7e728",
+    "url": "/React-supaMarket/static/js/1.28cdc5a0.chunk.js"
+  },
+  {
+    "revision": "b3b061a64c672babb0df",
+    "url": "/React-supaMarket/static/js/runtime~main.b3b061a6.js"
+  },
+  {
+    "revision": "778f41f2b62585db19b77de4b11204d7",
+    "url": "/React-supaMarket/index.html"
+  }
+];
